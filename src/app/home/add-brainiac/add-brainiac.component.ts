e0b8aa6835a447daa8079ae966ef9ed01576b533
Allnnka <Alionka)#)&00>
2021-12-14 22:37:35 +0100

    add validation for form

--- src/app/home/add-brainiac/add-brainiac.component.ts
+++ src/app/home/add-brainiac/add-brainiac.component.ts
@@ -1,10 +1,8 @@
-import { AfterViewInit, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { UserRequestPayload } from 'src/app/shared/user-request.payload';
-import { User } from 'src/app/shared/user.payload';
 import { UserService } from 'src/app/shared/user.service';
-import { TableComponent } from '../table/table.component';
 
 @Component({
   selector: 'add-brainiac',
@@ -26,7 +24,7 @@ export class AddBrainiacComponent implements OnInit {
       email:''
     }
   }
-
+  
   ngOnInit(): void {
     if(this.editUser===undefined){
       this.brainiacForm= new FormGroup({
@@ -39,28 +37,33 @@ export class AddBrainiacComponent implements OnInit {
     this.brainiacForm= new FormGroup({
       firstName:new FormControl(this.editUser.first_name,Validators.required),
       lastName:new FormControl(this.editUser.last_name,Validators.required),
-      email:new FormControl(this.editUser.email,[Validators.email , Validators.required])
+      email:new FormControl(this.editUser.email,[Validators.required,Validators.email])
     })
    }
   }
 
   addBrainiac(){
-    this.brainiacPayload.first_name= this.brainiacForm.get('firstName').value;
-    this.brainiacPayload.last_name= this.brainiacForm.get('lastName').value;
-    this.brainiacPayload.email= this.brainiacForm.get('email').value;
-
-    if(this.editUser===undefined){
-      this.userService.createUser(this.brainiacPayload).subscribe(()=>{
-        console.log("Brainiac create"); 
-        this.newUserEvent.emit(this.brainiacPayload);
-      })
+    if(this.brainiacForm.valid){
+      this.brainiacPayload.first_name= this.brainiacForm.get('firstName').value;
+      this.brainiacPayload.last_name= this.brainiacForm.get('lastName').value;
+      this.brainiacPayload.email= this.brainiacForm.get('email').value;
+  
+      if(this.editUser===undefined){
+        this.userService.createUser(this.brainiacPayload).subscribe(()=>{
+          console.log("Brainiac create"); 
+          this.newUserEvent.emit(this.brainiacPayload);
+        })
+      }
+      else{
+        this.userService.editUser(this.brainiacPayload).subscribe(()=>{
+          console.log("Brainiac edit"); 
+          Object.assign(this.editUser,this.brainiacPayload);
+        })
+      }
+      this.activeModal.close();
     }
     else{
-      this.userService.editUser(this.brainiacPayload).subscribe(()=>{
-        console.log("Brainiac edit"); 
-        Object.assign(this.editUser,this.brainiacPayload);
-      })
+      this.brainiacForm.markAllAsTouched();
     }
-    this.activeModal.close();
   }
 }
